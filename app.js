a2f3683d7e23639aa7963da0b2e806222c145dc1
bennkemboi <benkemboi23@gmail.com>
2023-05-10 23:51:40 +0300

    Add update to ejs-challenge

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const { now } = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -18,6 +19,7 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 // GLobal variable (save posts)
 let posts = [];
 
@@ -38,16 +40,28 @@ app.get("/compose", function (req, res) {
   res.render("compose");
 });
 //? Post title and text message
-app.post("/compose", function (rgq, res) {
+app.post("/compose", function (req, res) {
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody,
   };
   //! Redirect to another route
-  posts.push(posts);
+  posts.push(post);
 
   //* Send back to home page
   res.redirect("/");
+});
+
+app.get("/posts/:postName", function (req, res) {
+  var requestedTitle = req.params.postName;
+
+  posts.forEach(function (post) {
+    var storeTitle = post.title;
+
+    if (storeTitle === requestedTitle) {
+      console.log("match found!");
+    }
+  });
 });
 
 app.listen(3000, function () {
